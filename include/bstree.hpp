96bb64e45741ecc7c69766de0f3d9e60488a891a
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-04-15 11:30:13 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -2,32 +2,34 @@
 #include <string>
 #include <iostream>
 namespace BSTree {
-    struct Node {
-        int data;
-        Node* left;
-        Node* right;
-        Node(int val);
-    };
-    class Tree {
-        Node* root;
-        auto print_elements(Node* curr, int space) const -> void;
-        auto deleting(Node*&curr) -> void;
-        auto straight_detour(Node *curr) const -> void;
-        auto back_detour(Node *curr) const -> void;
-        auto transverse_detour(Node *curr) const -> void;
-        auto save_tree_to_the_file_recursion (Node * curr, int  space, std::ostream &File) ->void;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val);
+};
+class Tree {
+    Node* root;
+    auto print_elements(Node* curr, int space) const -> void;
+    auto deleting(Node*&curr) -> void;
+    auto straight_detour(Node *curr) const -> void;
+    auto back_detour(Node *curr) const -> void;
+    auto transverse_detour(Node *curr) const -> void;
+    auto save_tree_to_the_file_recursion (Node * curr, int  space, std::ostream &File) ->void;
+    auto save_tree_to_the_file_straight_detour (Node * curr, std::ostream &File) -> void;
 
-        public:
-        Tree();
-        auto insert(int value) -> bool;
-        auto print() const -> void;
-        auto straight() const -> void;
-        auto back() const -> void;
-        auto transverse() const -> void;
-        auto empty() const -> bool;
-        auto add_node (int value) -> void;
-        auto delete_node (int value) -> bool;
-        auto save_tree_to_the_file (std::string file_name) -> bool;
-        ~Tree();
-    };
+public:
+    Tree();
+    auto insert(int value) -> bool;
+    auto print() const -> void;
+    auto straight() const -> void;
+    auto back() const -> void;
+    auto transverse() const -> void;
+    auto empty() const -> bool;
+    auto add_node (int value) -> void;
+    auto delete_node (int value) -> bool;
+    auto save_tree_to_the_file (std::string file_name) -> bool;
+    auto download_tree_from_the_file (std::string file_name)->bool;
+    ~Tree();
+};
 }
