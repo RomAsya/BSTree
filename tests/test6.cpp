0f1eef36c6ca321f0b938a8f1d2f880398a52280
RomAsya <38328671+RomAsya@users.noreply.github.com>
2018-04-20 02:46:23 +0300

    Update test6.cpp

--- tests/test6.cpp
+++ tests/test6.cpp
@@ -24,7 +24,55 @@ TEST_CASE("save_tree_to_the_file")
   out.close();
   
   std::string result;
-  std::string text = "5 1 2 3 4 9 8 7 6";
+  std::string text = "5 1 2 3 4 9 8 7 6 ";
+  std::ifstream in("file.txt");
+  getline(in, result);
+  
+  REQUIRE(result == text);
+}
+
+TEST_CASE("save_tree_to_the_file")
+{
+	Tree tree;
+  tree.insert(1);
+  tree.insert(2);
+  tree.insert(3);
+  tree.insert(4);
+  tree.insert(5);
+  tree.insert(6);
+  tree.insert(7);
+  tree.insert(8);
+  tree.insert(9);
+  std::ofstream out("file.txt");
+  
+  tree.save_tree_to_the_file ("file.txt");
+  out.close();
+  
+  std::string result;
+  std::string text = "1 2 3 4 5 6 7 8 9 ";
+  std::ifstream in("file.txt");
+  getline(in, result);
+  
+  REQUIRE(result == text);
+}
+
+TEST_CASE("save_tree_to_the_file")
+{
+	Tree tree;
+  tree.insert(4);
+  tree.insert(3);
+  tree.insert(6);
+  tree.insert(8);
+  tree.insert(9);
+  tree.insert(5);
+  tree.insert(2);
+  std::ofstream out("file.txt");
+  
+  tree.save_tree_to_the_file ("file.txt");
+  out.close();
+  
+  std::string result;
+  std::string text = "4 3 2 6 5 8 9 ";
   std::ifstream in("file.txt");
   getline(in, result);
   
